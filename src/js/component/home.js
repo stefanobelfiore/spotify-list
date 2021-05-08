43380310bae9748577a683d7b40a2c813de97db3
stefanobelfiore <stefanobelfiore@yahoo.com>
2021-05-08 19:57:20 +0000

    falta mucho, de momento imprimo los nombres de la canciones

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,8 +3,11 @@ import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
-	const [listOfSongs, setListOfSongs] = useState([]);
-	console.log(listOfSongs);
+	const [listOfSongs, setListOfSongs] = useState([]); //----------------Array cada fichero canción
+	console.log(listOfSongs, "fafafafafafaf");
+	const [urlSong, setUrlSong] = useState(""); //--------------------string url cada canción
+	const [playSong, setplaySong] = useState(false); //----------------------estado button play/pause
+
 	useEffect(() => {
 		getSongs();
 	}, []);
@@ -26,40 +29,53 @@ export function Home() {
 				console.log("Looks like there was a problem: \n", error);
 			});
 	}
-	// const secondFetch = song => {
-	// 	fetch("https://assets.breatheco.de/apis/sound/".concat(song[0].url), {
-	// 		method: "GET",
-	// 		mode: "no-cors",
-	// 		redirect: "follow"
-	// 	})
-	// 		.then(function(response) {
-	// 			if (!response.ok) {
-	// 				console.log(response);
-	// 				throw Error("Ha ido mal");
-	// 			}
-	// 			return response.json();
-	// 		})
-	// 		.then(function(music) {
-	// 			console.log(music);
-	// 		})
-	// 		.catch(function(error) {
-	// 			console.log(error);
-	// 		});
-	// };
+
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+			{listOfSongs.map(oneSong => {
+				return (
+					<div
+						className={"https://assets.breatheco.de/apis/sound/".concat(
+							oneSong.url
+						)}
+						// key={oneSong.url}????????????????
+						onClick={() => {
+							setUrlSong(
+								"https://assets.breatheco.de/apis/sound/".concat(
+									oneSong.url
+								)
+							);
+
+							setplaySong(true);
+						}}>
+						{oneSong.name}
+					</div>
+				);
+			})}
 		</div>
 	);
 }
+
+//----------------------------------------------------------------------------------------------------------------------------------
+// const secondFetch = song => {
+// 	fetch("https://assets.breatheco.de/apis/sound/".concat(song[0].url), {
+// 		method: "GET",
+// 		mode: "no-cors",
+// 		redirect: "follow"
+// 	})
+// 		.then(function(response) {
+// 			if (!response.ok) {
+// 				console.log(response);
+// 				throw Error("Ha ido mal");
+// 			}
+// 			return response.json();
+// 		})
+// 		.then(function(music) {
+// 			console.log(music);
+// 		})
+// 		.catch(function(error) {
+// 			console.log(error);
+// 		});
+// };
